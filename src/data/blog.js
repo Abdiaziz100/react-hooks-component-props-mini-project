21d4a4ec19c88541a21422ba908cc7de3928ea21
mohamed <abdiazizmahat@gmail.com>
2025-07-28 20:48:31 +0300

    special

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,30 +1,16 @@
-import logo from "../assets/logo";
-
 const blogData = {
-  name: "Underreacted",
-  image: logo,
-  about: "A blog about learning React",
+  name: "My Blog",
+  image: "https://via.placeholder.com/215",
+  about: "All things React and JavaScript.",
   posts: [
     {
       id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
+      title: "Getting Started",
+      date: "July 1, 2025",
+      preview: "Let’s dive into the basics of React.",
       minutes: 5,
     },
-    {
-      id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
-    },
-    {
-      id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
-    },
+    // ... more posts
   ],
 };
 
